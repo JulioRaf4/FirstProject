70fec871af070204200ac75c2dbbbd20d1fb9ec1
Julio Rafael <96303574+JulRafael@users.noreply.github.com>
2021-12-21 21:07:19 -0300

    Add files via upload

---
+++ advinhação.py
@@ -0,0 +1,57 @@
+import  random
+def jogar():
+
+    
+    escolha = -1
+    tentativa = 0
+    sorte = random.randint(0,100)
+    invalido = False
+
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+
+    while invalido == False :
+        print("INSIRA UMA DIFICULDADE")
+        print("(1)Facil - (2) Médio - (3) Difícil")
+        dificuldade = int(input())
+        if dificuldade > 0 and dificuldade < 4:
+            invalido = True
+
+    if dificuldade == 1:
+        jogadas = 20 
+        print("[MODO FACÍL ESCOLHIDO]")
+    elif dificuldade == 2:
+        jogadas = 10
+        print("[MODO MÉDIO ESCOLHIDO]")
+
+    elif dificuldade == 3:
+        jogadas = 5
+        print("[MODO HARD ESCOLHIDO]")   
+
+    while jogadas > tentativa :
+        print("Faça sua escolha:", end=" ")
+        escolha = int(input())
+        tentativa += 1
+        print("{}º Tentativa".format(tentativa))
+        if sorte > escolha and escolha >= 0:
+            print("Mais... Tente outra vez")
+
+        elif sorte < escolha and escolha <= 100:
+            print("Menos... Tente outra vez")
+    
+        elif sorte == escolha:
+            print("Parabéns!")
+            print("Você ganhou!, o número sorteado foi:",sorte )
+            print("Quantidade de tentavivas:",tentativa)  
+
+        else:
+            print("Insira um valor entre 0 e 100")
+            
+    if sorte != escolha:
+        print("Você perdeu!")
+        print("Quantidade de tentavivas:",tentativa) 
+        print("Número sorteado foi:", sorte)
+
+if __name__ == "__main__":
+    jogar()
---
+++ jogos.py
@@ -0,0 +1,17 @@
+import Forca
+import advinhação
+
+print("*********************************")
+print("*******ESCOLHA SEU JOGO!*********")
+print("*********************************")
+
+print("(1) PARA FORCA (2)PARA ADVINHAÇÃO")
+jogo = int(input())
+
+if jogo == 1:
+    print("JOGANDO FORCA")
+    Forca.jogar()
+
+elif jogo == 2:
+    print("JOGANDO ADIVINHAÇÃO")    
+    advinhação.jogar()
